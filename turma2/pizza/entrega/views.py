1614a5cf95fc7955bbbe1bcb530041e4e0bc1c60
Luciano Ramalho <luciano@ramalho.org>
2013-04-22 18:16:38 -0300

    separando repos das turmas 1 e 2

--- turma2/pizza/entrega/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render_to_response
-
-from .models import Pizza
-
-def pizzas_pendentes(request):
-    lista = Pizza.objects.all().order_by('pedido')[:10]
-    contexto = {'lista': lista}
-    return render_to_response('entrega/pizzas_pend.html', contexto)
