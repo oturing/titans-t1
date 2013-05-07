853a3733a0c94a42a682bc006ce5fa318918ba6f
Luciano Ramalho <luciano@ramalho.org>
2013-05-06 21:50:58 -0300

    primeira view funcionando com mongodb

---
+++ modulo2/flaskr_mongo/flaskr.py
@@ -0,0 +1,76 @@
+# coding: utf-8
+
+# todos os imports
+from contextlib import closing
+import pymongo
+from flask import Flask, request, session, g, redirect, url_for, \
+     abort, render_template, flash
+
+# configuração
+DATABASE = 'flaskr'
+DEBUG = True
+SECRET_KEY = 'development key'
+USERNAME = 'admin'
+PASSWORD = 'default'
+
+# criar nossa pequena aplicação :)
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+################################################# Configuração
+
+def conectar_bd():
+    cliente = pymongo.MongoClient()
+    return cliente[app.config['DATABASE']]
+
+@app.before_request
+def pre_requisicao():
+    g.bd = conectar_bd()
+
+@app.teardown_request
+def encerrar_requisicao(exception):
+    g.bd.connection.close()
+
+
+################################################# Views
+
+@app.route('/')
+def exibir_entradas():
+    entradas = g.bd.entradas.find()
+
+    return render_template('exibir_entradas.html', entradas=entradas)
+
+@app.route('/entrar', methods=['GET', 'POST'])
+def login():
+    erro = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            erro = u'Usuário inválido'
+        elif request.form['password'] != app.config['PASSWORD']:
+            erro = u'Senha inválida'
+        else:
+            session['logado'] = True
+            flash('Login OK')
+            return redirect(url_for('exibir_entradas'))
+    return render_template('login.html', erro=erro)
+
+@app.route('/sair')
+def logout():
+    session.pop('logado', None)
+    flash('Logout OK')
+    return redirect(url_for('exibir_entradas'))
+
+@app.route('/inserir', methods=['POST'])
+def inserir_entrada():
+    if not session.get('logado'):
+        abort(401)
+    sql = '''insert into entradas (titulo, texto) values (?, ?)'''
+    g.bd.execute(sql, [request.form['titulo'], request.form['texto']])
+    g.bd.commit()
+    flash('Nova entrada registrada com sucesso')
+    return redirect(url_for('exibir_entradas'))
+
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ modulo2/flaskr_mongo/flaskr_tests.py
@@ -0,0 +1,69 @@
+# coding: utf-8
+
+from __future__ import unicode_literals
+
+import unittest
+from datetime import datetime
+
+import pymongo
+
+import flaskr
+
+
+class FlaskrTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.nome_bd = str(datetime.now()).replace('.','_').replace(' ','_')
+        flaskr.app.config['DATABASE'] = self.nome_bd
+        flaskr.app.config['TESTING'] = True
+        self.app = flaskr.app.test_client()
+
+    def tearDown(self):
+        '''apagar BD de teste no MongoDB'''
+
+    def teste_conexao_bd(self):
+        bd = flaskr.conectar_bd()
+        self.assertIsInstance(bd, pymongo.database.Database)
+
+    def teste_bd_vazio(self):
+        res = self.app.get('/')
+        self.assertIn(b'nenhuma entrada', res.data)
+
+    def entrar(self, usuario, senha):
+        return self.app.post('/entrar', data=dict(
+                username=usuario,
+                password=senha
+            ), follow_redirects=True)
+
+    @unittest.skip('migrar para MongoDB')
+    def teste_login(self):
+        rv = self.entrar('admin', 'default')
+        self.assertIn(b'Login OK', rv.data)
+
+    @unittest.skip('migrar para MongoDB')
+    def teste_login_invalido(self):
+        rv = self.entrar('adminXXX', 'default')
+        self.assertIn(b'Usuário inválido', rv.data)
+        rv = self.entrar('admin', 'defaultYYY')
+        self.assertIn(b'Senha inválida', rv.data)
+
+    @unittest.skip('migrar para MongoDB')
+    def teste_sair(self):
+        rv = self.app.get('/sair', follow_redirects=True)
+        self.assertIn(b'Logout OK', rv.data)
+
+    @unittest.skip('migrar para MongoDB')
+    def teste_nova_entrada(self):
+        self.entrar('admin', 'default')
+        rv = self.app.post('/inserir', data=dict(
+            titulo='<Olá>',
+            texto='<strong>HTML</strong> é permitido aqui'
+        ), follow_redirects=True)
+        self.assertEquals(rv.status_code, 200)
+        self.assertNotIn(b'nenhuma entrada', rv.data)
+        self.assertIn(b'&lt;Olá&gt;', rv.data)
+        self.assertIn(b'<strong>HTML</strong> é permitido aqui', rv.data)
+
+
+if __name__ == '__main__':
+    unittest.main()
