1eb8c2e7014e53398d65dcf2811b4f4e1d076008
Luciano Ramalho <luciano@ramalho.org>
2013-05-13 21:13:58 -0300

    arquivos faltantes

---
+++ modulo2/flaskr2_sqlalchemy/forms.py
@@ -0,0 +1,10 @@
+#coding: utf-8
+
+from wtforms import Form, TextField, TextAreaField, validators
+
+class ComentarioForm(Form):
+    nome = TextField(u'Nome', [validators.Length(min=4, max=32)])
+    email = TextField(u'Email', [validators.Email()])
+    comentario = TextAreaField(u'Comentário',
+                    [validators.Length(min=12, max=1000)])
+
