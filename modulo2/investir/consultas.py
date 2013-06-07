9e9d6c5b8825e0c9fe054445eefa6a78e2d4aeef
Luciano Ramalho <luciano@ramalho.org>
2013-06-06 21:57:52 -0300

    relacionentos e primeiro join

--- modulo2/investir/consultas.py
+++ modulo2/investir/consultas.py
@@ -5,7 +5,10 @@ import io, os, csv, cStringIO
 import sqlalchemy as sa
 from sqlalchemy.orm import sessionmaker
 
-from modelos import Cliente
+# operadores de Query
+from sqlalchemy import or_, func
+
+from modelos import Cliente, Acao, CarteiraAcoes
 
 def clientes(sessao):
     """
@@ -27,19 +30,115 @@ def acoes_tecnologia(sessao):
     for acao in res:
         print acao.simbolo
 
+def acoes_cotacao_desc(sessao):
+    """
+    E3. Listar simbolos e cotacoes das três ações mais caras, acoes em
+        ordem decrescente de cotacao
+    """
+    res = (sessao.query(Acao.simbolo, Acao.cotacao)
+                 .order_by(Acao.cotacao.desc())[:3]
+          )
+    for acao in res:
+        print acao.simbolo, acao.cotacao
+
+def acoes_alimentos_ou_farma(sessao):
+    """
+    E4. Listar simbolos das acoes do setor "alimentos" ou "farmaceutica"
+    """
+    res = (sessao.query(Acao.simbolo)
+                 .filter(or_(Acao.setor == "alimentos",
+                             Acao.setor == "farmaceutica")
+                        )
+          )
+    for acao in res:
+        print acao.simbolo
+
+def acao_cotacao_23(sessao):
+    """
+    E5. Exibir simbolo e cotacao da ação com cotação = 23
+    """
+    query = (sessao.query(Acao.simbolo, Acao.cotacao)
+                          .filter_by(cotacao=23))
+    try:
+        acao = query.one()
+        print acao.simbolo, acao.cotacao
+    except sa.orm.exc.MultipleResultsFound:
+        print 'Não existe apenas uma ação com cotação = 23.'
+        print 'Exibindo a primeira delas:'
+        acao = query.first()
+        print acao.simbolo, acao.cotacao
+
+def cotacao_minima(sessao):
+    """
+    E6. Exibir menor valor de cotação de todas as ações
+    """
+    cotacao = sessao.query(func.min(Acao.cotacao)).one()[0]
+    print cotacao
+
+def acoes_mais_caras(sessao):
+    """
+    E7. Exibir simbolo e cotacao de todas as ações com a menor cotação
+    """
+    res = (sessao.query(Acao.simbolo, Acao.cotacao)
+                 .filter(Acao.cotacao.in_(sessao.query(func.max(Acao.cotacao))))
+          )
+    for acao in res:
+        print acao.simbolo, acao.cotacao
+
+def acoes_do_cliente_6_ineficiente(sessao):
+    """
+    E8i. Exibir simbolo, cotacao e cotas de todas as ações do cliente com
+        cod_contribuinte = 6 (implementação altamente ineficiente)
+    """
+    cliente6 = (sessao.query(Cliente).filter_by(cod_contribuinte = 6).one())
+    for carteira in cliente6.carteiras:
+        print carteira.acao.simbolo, carteira.acao.cotacao, carteira.cotas
+
+def acoes_do_cliente_6(sessao):
+    """
+    E8. Exibir simbolo, cotacao e cotas de todas as ações do cliente com
+        cod_contribuinte = 6 (implementação altamente ineficiente)
+    """
+    carteira_cliente6 = (sessao.query(CarteiraAcoes.simbolo, CarteiraAcoes.cotas, Acao.cotacao)
+                               .filter_by(cod_contribuinte = 6)
+                               .join(Acao))
+    for acao in carteira_cliente6:
+        print acao.simbolo, acao.cotas, acao.cotacao
+
 def criar_sessao(url_bd, echo=False):
     engine = sa.create_engine(url_bd, echo=echo)
     cnx = engine.connect()
     Session = sessionmaker(bind=engine)
     return Session(bind=cnx)
 
-def main():
+def listar_consultas():
+    """
+    Devolve o nome e a referência de todas as funções definidas ou importadas
+    neste módulo que tenham o primeiro argumento chamado `sessao`
+    """
+    consultas = {}
+    for chave, valor in globals().items():
+        if hasattr(valor, '__code__') and valor.__code__.co_varnames[0] == 'sessao':
+            consultas[chave] = valor
+    return consultas
+
+def main(argv):
+    """
+    Executa qualquer função definida ou importada neste módulo que tenha
+    o primeiro argumento chamado `sessao`
+    """
+    consultas = listar_consultas()
+    if len(argv) != 2 or argv[1] not in consultas:
+        print 'Informe a consulta a ser executada:'
+        for nome in sorted(consultas):
+            print '\t', nome
+        raise SystemExit
+
     url = 'sqlite:///investir.sqlite'
-    sessao = criar_sessao(url)
-
-    #clientes(sessao)
-    acoes_tecnologia(sessao)
-
+    sessao = criar_sessao(url, echo=True)
+    # executar a consulta
+    consultas[argv[1]](sessao)
 
 if __name__=='__main__':
-    main()
+    import sys
+    main(sys.argv)
--- modulo2/investir/modelos.py
+++ modulo2/investir/modelos.py
@@ -1,7 +1,8 @@
 # coding: utf-8
 
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship, backref
 
 Base = declarative_base()
 
@@ -11,4 +12,34 @@ class Cliente(Base):
     cod_contribuinte = Column(String, primary_key=True)
     nome = Column(String)
     endereco = Column(String)
+
+# @acao(simbolo/char,empresa/char,avaliacao/char,setor/char,
+# cotacao_max/numeric,cotacao_min/numeric,cotacao/numeric,retorno_1a/numeric,retorno_5a/numeric):simbolo
+
+class Acao(Base):
+    __tablename__ = 'acao'
+
+    simbolo = Column(String, primary_key=True)
+    empresa = Column(String)
+    setor = Column(String)
+    cotacao_max = Column(Integer)
+    cotacao_min = Column(Integer)
+    cotacao = Column(Integer)
+    retorno_1a = Column(Integer)
+    retorno_5a = Column(Integer)
+
+# @carteira_acoes(cod_contribuinte/char,simbolo/char,cotas/numeric):cod_contribuinte,simbolo
+
+class CarteiraAcoes(Base):
+    __tablename__ = 'carteira_acoes'
+
+    cod_contribuinte = Column(String, ForeignKey('cliente.cod_contribuinte'), primary_key=True)
+    simbolo = Column(String, ForeignKey('acao.simbolo'), primary_key=True)
+    cotas = Column(Integer)
+
+    # relacionamentos
+    cliente = relationship('Cliente', backref=backref('carteiras', order_by=simbolo))
+    acao = relationship('Acao', backref=backref('carteiras', order_by=cod_contribuinte))
+
+
 
